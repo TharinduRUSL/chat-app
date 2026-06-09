3f086d8789f881b89b8ed60cd5fec1a3114b118b
Tharindu Sampath <sampaththarindu119@gmail.com>
2026-06-09 09:45:05 -0700

    Create first REST API endpoint

---
+++ backend/src/main/java/com/chatapp/backend/entity/Message.java
@@ -0,0 +1,17 @@
+package com.chatapp.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String content;
+}
